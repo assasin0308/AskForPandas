5bfb202d8810f1eae6e238b31d5b6813221e7f72
assasin0308 <assasin0308@sina.com>
2024-10-13 01:02:16 +0800

    熊猫问答社区钩子函数的处理与上下文信息设置

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # 熊猫解惑 Ask For Pandas
-from flask import Flask
+from flask import Flask,session,g
 import config
 from exts import db
 from exts import mail
@@ -21,18 +21,33 @@ mail.init_app(app)
 # 数据库迁移
 migrate = Migrate(app,db)
 
-
-
-
-
-
 #注册蓝图
 app.register_blueprint(qa_bp)
 app.register_blueprint(auth_bp)
 
+
+# before_request/before_first_request/after_request
+# hook 钩子函数的使用
+
+@app.before_request
+def app_before_request():
+    user_id = session.get("user_id")
+    if user_id:
+        user = UserModel.query.get(user_id)
+        setattr(g,"user",user)
+    else:
+        setattr(g, "user", None)
+
+@app.context_processor
+def app_context_processor():
+    return {"user": g.user}
 
 
 if __name__ == '__main__':
     # 路由列表
     print(app.url_map)
     app.run(host='0.0.0.0',port=5000,debug=True)
+
+
+
+# https://www.bilibili.com/video/BV17r4y1y7jJ?p=31&spm_id_from=pageDriver&vd_source=d2f655c567775b67c1169ee3673e2916
