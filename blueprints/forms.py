dd14c10107bd219a29fc4d4ca60f3d7e56a1bfb8
assasin0308 <assasin0308@sina.com>
2024-10-13 13:37:03 +0800

    熊猫问答社区发布问答

--- blueprints/forms.py
+++ blueprints/forms.py
@@ -42,4 +42,8 @@ class LoginForm(wtforms.Form):
     password = wtforms.StringField(validators=[Length(min=6, max=20, message="密码格式错误!")])
 
 
+class QuestionForm(wtforms.Form):
+    title = wtforms.StringField(validators=[Length(min=6, max=100, message="标题格式错误!")])
+    content = wtforms.StringField(validators=[Length(min=6, message="内容格式错误!")])
+
 
--- blueprints/qa.py
+++ blueprints/qa.py
@@ -1,4 +1,10 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,g,redirect,url_for
+from .forms import QuestionForm
+from models import QuestionModel
+from exts import db
+
+
+
 
 bp = Blueprint("qa",__name__,url_prefix="/")
 
@@ -9,6 +15,21 @@ def index():
     return render_template("index.html")
 
 @bp.route("/qa/publish",methods = ['GET','POST'])
-def publish_qa():
+def publish_question():
     if request.method == 'GET':
         return render_template('public_question.html')
+    else:
+        form = QuestionForm(request.form)
+        if form.validate():
+            title = form.title.data
+            content = form.content.data
+            question = QuestionModel(title=title,content=content,author=g.user)
+            db.session.add(question)
+            db.session.commit()
+            # TODO 跳转到这篇问答详情页
+
+            return redirect("/")
+        else:
+            print(form.errors)
+            return redirect(url_for("qa.publish_question"))
+
